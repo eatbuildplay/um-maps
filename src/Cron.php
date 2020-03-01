e73a7d5beafe48142d837cee029fa694e14ae83e
Casey Milne <joel@goldhat.ca>
2020-03-01 04:05:50 -0600

    updating cron class

--- src/Cron.php
+++ src/Cron.php
@@ -4,7 +4,20 @@ namespace UM_MAPS;
 
 class CronGeocode {
 
+  public function __construct() {
+
+    add_action( $this->actionKey, array( $this, 'run'));
+
+  }
+
   public $actionKey = 'um_maps_geocoding';
+
+  public function run() {
+
+    $geocode = new PP_Geocode;
+    $geocode->pp_bulk_geocode();
+
+  }
 
   public function onActivation() {
     wp_schedule_event( time(), 'daily', $this->actionKey );
